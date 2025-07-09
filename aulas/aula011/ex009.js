d4a7fbf9b46fbce3ea0490d81d70581f77d1aeef
Dev joao <jaowsaviao@gmail.com>
2025-07-09 12:00:22 -0300

    novo

---
+++ aulas/aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') { //neste caso hoje em dia, caso faça pela web use: pais.value
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
