d9450e38d72bd4628eb31c7a1c29878aa5e74c75
Dev joao <jaowsaviao@gmail.com>
2025-07-20 13:14:05 -0300

    novo

--- aulas/aula015/ambiente.js
+++ aulas/aula015/ambiente.js
@@ -5,7 +5,7 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 let pos = num.indexOf(8)
-if (post == -1 ) {
+if (pos == -1 ) {
     console.log('O valor não foi encontrado!')
 } else {
     console.log(`o valor 8 está na posição ${pos}`)
